d546b4dc473c9cc614ad34cfb2d2b01d9c175724
Uttam K N <124062691+uttamkn@users.noreply.github.com>
2024-12-06 14:54:34 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold text-red-400 underline">
+        hello world
+      </h1>
+    </>
+  );
+}
+
+export default App;
